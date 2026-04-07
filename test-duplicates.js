736c7c3d88a0b950de936d87a6e5f78617268df3
collyn <collyn094@gmail.com>
2026-04-07 14:11:39 +0700

    chore: remove obsolete test and debugging scripts

--- test-duplicates.js
+++
@@ -1,5 +0,0 @@
-const { readFileSync } = require('fs');
-
-// We need to look at data/hardware-presets.ts
-const code = readFileSync('src/data/hardware-presets.ts', 'utf8');
-console.log(code.substring(0, 1000));
